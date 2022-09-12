54eab0dc9073c2d84967c27be48f08f101e570c8
Abdulrazak Othman <zaak0101@hotmail.com>
2022-09-13 02:12:27 +0300

    add support etl ci/cd

---
+++ auto_etl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python3 ./code/sakani_etl.py #extraction, transformation and loading data
+echo "Finished ETL Proccess.!"
+sleep 5
+
+#after we load the data to sqlite3 db, we converting it to (.sql) file path to make it eaiser,
+#for us to load it to multiple DWH.
+sqlite3 db/jobseekers.db .dump > db/jobseekers.sql
+echo "Finished Converting to SQL File.!"
