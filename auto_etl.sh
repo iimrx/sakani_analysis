186fd990be9042b5dc7473d678daac34cf48f4f4
Abdulrazak Othman <zaak0101@hotmail.com>
2022-09-13 05:50:38 +0300

    fixing sqlit3 bug related

--- auto_etl.sh
+++ auto_etl.sh
@@ -6,5 +6,5 @@ sleep 5
 
 #after we load the data to sqlite3 db, we converting it to (.sql) file path to make it eaiser,
 #for us to load it to multiple DWH.
-sqlite3 db/jobseekers.db .dump > db/jobseekers.sql
+sqlite3 db/sakani.db .dump > db/sakani.sql
 echo "Finished Converting to SQL File.!"
